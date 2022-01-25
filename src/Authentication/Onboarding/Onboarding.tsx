d726ea31e0d75dbf9091a3b860515cf62469cf01
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-25 15:07:56 +0700

    a: Transform & Rotate

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,10 +1,59 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
-const ComponentName = () => {
+import Slide from "./Slide";
+
+const { width, height } = Dimensions.get("window");
+const Onboarding = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: "cyan" }} />
+    <View style={styles.container}>
+      <View style={styles.slider}>
+        <ScrollView
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+        >
+          <Slide label="Relaxed" />
+          <Slide label="Playful" right />
+          <Slide label="Excentric" />
+          <Slide label="Funky" right />
+        </ScrollView>
+      </View>
+      <View style={styles.footer}>
+        <View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor: "cyan",
+          }}
+        />
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "white",
+            borderTopLeftRadius: 75,
+          }}
+        />
+      </View>
+    </View>
   );
 };
 
-export default ComponentName;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  slider: {
+    height: 0.61 * height,
+    backgroundColor: "cyan",
+    borderBottomRightRadius: 75,
+  },
+  footer: {
+    flex: 1,
+  },
+});
+
+export default Onboarding;
