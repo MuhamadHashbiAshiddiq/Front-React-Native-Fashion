2bdffa14eb34f7062b1f09ac6673f1e9a88e3af6
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-25 16:27:56 +0700

    a: Scroll Handler

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,39 +1,62 @@
 import React from "react";
+import Animated from "react-native-reanimated";
+
 import {
-  Animated,
   ScrollView,
   View,
   StyleSheet,
   Dimensions,
 } from "react-native";
-import { useValue } from "react-native-redash";
+
+import {
+  interpolateColor,
+  onScrollEvent,
+  useValue,
+} from "react-native-redash";
 
 import Slide from "./Slide";
 
 const { width, height } = Dimensions.get("window");
 const Onboarding = () => {
   const x = useValue(0);
+
+  // useScrollEvents
+  const backgroundColor = interpolateColor(x, {
+    inputRange: [0, width, width * 2, width * 3],
+    outputRange: [
+      "#BFEAF5",
+      "#BEECC4",
+      "#FFE4D9",
+      "#FFDDDD",
+    ],
+  });
+
+  const onScroll = onScrollEvent({ x });
+
   return (
     <View style={styles.container}>
-      <View style={styles.slider}>
+      <Animated.View
+        style={[styles.slider, { backgroundColor }]}
+      >
         <Animated.ScrollView
           horizontal
           snapToInterval={width}
           decelerationRate="fast"
           showsHorizontalScrollIndicator={false}
           bounces={false}
+          {...{ onScroll }}
         >
           <Slide label="Relaxed" />
           <Slide label="Playful" right />
           <Slide label="Excentric" />
           <Slide label="Funky" right />
         </Animated.ScrollView>
-      </View>
+      </Animated.View>
       <View style={styles.footer}>
-        <View
+        <Animated.View
           style={{
             ...StyleSheet.absoluteFillObject,
-            backgroundColor: "cyan",
+            backgroundColor,
           }}
         />
         <View
@@ -51,11 +74,10 @@ const Onboarding = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    // backgroundColor: "black",
   },
   slider: {
     height: 0.61 * height,
-    backgroundColor: "cyan",
     borderBottomRightRadius: 75,
   },
   footer: {
